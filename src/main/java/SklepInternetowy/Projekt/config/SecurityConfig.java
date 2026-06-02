b0b1ca6a1bed6840ddb04e91be6530ae53971add
Pawel <p.ostapinski97@gmail.com>
2026-06-02 20:47:54 +0200

    Dodanie ustawien testowych

--- src/main/java/SklepInternetowy/Projekt/config/SecurityConfig.java
+++ src/main/java/SklepInternetowy/Projekt/config/SecurityConfig.java
@@ -2,7 +2,7 @@ package SklepInternetowy.Projekt.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+//import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -14,19 +14,25 @@ public class SecurityConfig {
             throws Exception {
 
         http
+                //USTAWIENIA POD TESTY
                 .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                .requestMatchers(
-                        "/api/auth/register",
-                        "/api/auth/login",
-                        "/api/auth/register",
-                        "/api/auth/login",
-                        "/swagger-ui/**",
-                        "/v3/api-docs/**"
-                ).permitAll()
-                .anyRequest().authenticated()
-                )
-                .httpBasic(Customizer.withDefaults());
+            .authorizeHttpRequests(auth -> auth
+                    .anyRequest().permitAll()
+            );
+        // NIE USUWAC TO DEFAULTOWE USTAWIENIA 
+//                .csrf(csrf -> csrf.disable())
+//                .authorizeHttpRequests(auth -> auth
+//                .requestMatchers(
+//                        "/api/auth/register",
+//                        "/api/auth/login",
+//                        "/api/auth/register",
+//                        "/api/auth/login",
+//                        "/swagger-ui/**",
+//                        "/v3/api-docs/**"
+//                ).permitAll()
+//                .anyRequest().authenticated()
+//                )
+//                .httpBasic(Customizer.withDefaults());
 
         return http.build();
     }
