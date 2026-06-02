4dd264ae9a607be57095df7d66acaf25ce8fb079
Pawel <p.ostapinski97@gmail.com>
2026-06-02 18:33:44 +0200

    Dodanie Funkcjonalności Logowanie oraz Rejestracja -dodanie konfigurajci początkowej spring securitu -dodanie konta admina -dodanie controllera logowania oraz rejestracji

--- src/main/java/SklepInternetowy/Projekt/repository/UserRep.java
+++ src/main/java/SklepInternetowy/Projekt/repository/UserRep.java
@@ -2,9 +2,13 @@
 package SklepInternetowy.Projekt.repository;
 
 import SklepInternetowy.Projekt.entity.UserEnt;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface UserRep extends JpaRepository<UserEnt,Long>{
     
+    Optional<UserEnt> findByEmail(String email);
+    
+    boolean existsByEmail(String email);
 }
