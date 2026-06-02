dc0c60c5e2edf02c794be7d12cb69471dfd95f87
Natalia <ruszniaczek@gmail.com>
2026-06-02 21:51:35 +0200

    Dodanie OrderItem ENT, Rep

---
+++ src/main/java/SklepInternetowy/Projekt/entity/OrderItemEnt.java
@@ -0,0 +1,78 @@
+package SklepInternetowy.Projekt.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "order_items")
+public class OrderItemEnt {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // do jakiego zamówienia należy
+    @ManyToOne
+    @JoinColumn(name = "order_id", nullable = false)
+    private OrderEnt order;
+
+    @Column(nullable = false)
+    private String productName;
+
+    @Column(nullable = false)
+    private double price;
+
+    @Column(nullable = false)
+    private int quantity;
+
+    public OrderItemEnt() {
+    }
+
+    public OrderItemEnt(OrderEnt order, String productName, double price, int quantity) {
+        this.order = order;
+        this.productName = productName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    // GETTERY I SETTERY
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public OrderEnt getOrder() {
+        return order;
+    }
+
+    public void setOrder(OrderEnt order) {
+        this.order = order;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
