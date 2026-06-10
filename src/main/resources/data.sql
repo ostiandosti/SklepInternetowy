0a0c3ae8ec2e840b06da9ec0dfac5e437a372ab5
Pawel <p.ostapinski97@gmail.com>
2026-06-10 08:28:18 +0200

    Dodanie bazy h2 na potrzeby jednego pliku startowego

---
+++ src/main/resources/data.sql
@@ -0,0 +1,42 @@
+
+INSERT INTO products (name, description, price, quantity, image_url)
+VALUES
+('The Witcher 3: Wild Hunt', 'RPG open-world osadzony w świecie fantasy', 89.99, 10, 'witcher3.jpg'),
+
+('Cyberpunk 2077', 'Futurystyczne RPG akcji w Night City', 119.99, 15, 'cyberpunk2077.jpg'),
+
+('Red Dead Redemption 2', 'Western open-world z fabułą', 129.99, 8, 'rdr2.jpg'),
+
+('Elden Ring', 'Trudne RPG akcji od FromSoftware', 199.99, 12, 'eldenring.jpg'),
+
+('Minecraft', 'Gra sandbox o budowaniu i przetrwaniu', 79.99, 50, 'minecraft.jpg'),
+
+('Grand Theft Auto V', 'Open-world akcja w Los Santos', 69.99, 30, 'gtav.jpg'),
+
+('Counter-Strike 2', 'Taktyczna strzelanka multiplayer FPS', 0.00, 100, 'cs2.jpg'),
+
+('League of Legends', 'MOBA 5v5 online', 0.00, 100, 'lol.jpg'),
+
+('Valorant', 'Taktyczny FPS od Riot Games', 0.00, 80, 'valorant.jpg'),
+
+('FIFA 24', 'Symulator piłki nożnej', 249.99, 20, 'fifa24.jpg'),
+
+('Call of Duty: Modern Warfare III', 'FPS kampania i multiplayer', 279.99, 25, 'codmw3.jpg'),
+
+('Assassin’s Creed Valhalla', 'Skandynawska przygoda asasyna', 99.99, 14, 'acvalhalla.jpg'),
+
+('Hogwarts Legacy', 'RPG w świecie Harry’ego Pottera', 149.99, 18, 'hogwarts.jpg'),
+
+('Starfield', 'Kosmiczne RPG Bethesdy', 199.99, 10, 'starfield.jpg'),
+
+('Diablo IV', 'Mroczne RPG hack & slash', 179.99, 22, 'diablo4.jpg'),
+
+('Fortnite', 'Battle royale z budowaniem', 0.00, 200, 'fortnite.jpg'),
+
+('The Sims 4', 'Symulator życia', 59.99, 40, 'sims4.jpg'),
+
+('Battlefield 2042', 'Wojenny FPS multiplayer', 89.99, 16, 'bf2042.jpg'),
+
+('Need for Speed Unbound', 'Wyścigi uliczne', 129.99, 11, 'nfsunbound.jpg'),
+
+('Baldur’s Gate 3', 'Rozbudowane RPG turowe', 219.99, 9, 'bg3.jpg');
